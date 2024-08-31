3879bcaaad3c4b8c31df0968f66eb398795ccc6e
gurdian0614 <ivan.gurdian@outlook.com>
2024-08-30 19:45:42 -0600

    Add project files.

---
+++ FormulaCuadraticaMVVMQ32024/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FormulaCuadraticaMVVMQ32024
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
